e0ffdda9acfa2433fc0ac534da0056e3544c1ecf
vivekky <vivekky57@gmail.com>
2022-10-13 03:08:55 +0530

    Animation Timing

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -2,11 +2,21 @@ import React from "react";
 import { Transition } from "react-transition-group";
 import "./Modal.css";
 
+const animationTiming = {
+  enter: 400, //we'll use for adding this elemrnt
+  exit: 1000, // exit defines the duration it will use for removing it.
+};
+
 const modal = (props) => {
   // const cssClasses = ["Modal"];
 
   return (
-    <Transition in={props.show} timeout={300} mountOnEnter unmountOnExit>
+    <Transition
+      in={props.show}
+      timeout={animationTiming}
+      mountOnEnter
+      unmountOnExit
+    >
       {(state) => {
         const cssClasses = [
           "Modal",
